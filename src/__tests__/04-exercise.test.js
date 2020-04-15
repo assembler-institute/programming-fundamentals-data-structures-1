9b4bf93f9d40e27e6e3a570afefe0b743ee726c0
Dani Lucaci <danilucaci@gmail.com>
2020-04-15 20:36:58 +0200

    fix ex 4 test

--- src/__tests__/04-exercise.test.js
+++ src/__tests__/04-exercise.test.js
@@ -2,9 +2,6 @@ const exercise04 = require('../04-exercise');
 
 describe('04-exercise', () => {
     test('add the new names to the names array', () => {
-        const actual = ['John', 'Mark', 'Ana', 'Maria'];
-        const expected = exercise04();
-
-        expect(actual).toEqual(expect.arrayContaining(expected));
+        expect(exercise04()).toHaveLength(4);
     });
 });
